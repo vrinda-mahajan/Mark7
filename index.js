27aaebe43567c5fb6a5853c956010234f5291bad
Vrinda Mahajan <vrindamahajan96@gmail.com>
2021-08-29 13:28:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/yoda.json";
+
+function getTranslationURL(text) {
+  return serverURL + "?text=" + text;
+}
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("Something wrong with server,Try Again Later!");
+}
+function handleClick() {
+  var inputText = txtInput.value;
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => (outputDiv.innerText = json.contents.translated))
+    .catch(errorHandler);
+}
+
+btnTranslate.addEventListener("click", handleClick);
